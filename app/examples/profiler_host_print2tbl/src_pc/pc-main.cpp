7187871b4a849ad05563c36e459d6ba3b1f475be
Sergey Mushkaev <mushkaev@module.ru>
2017-11-16 23:14:09 +0300

    srx

---
+++ app/examples/profiler_host_print2tbl/src_pc/pc-main.cpp
@@ -0,0 +1,35 @@
+#include "ubc_host.h"
+#include "stdio.h"
+#include "windows.h"
+
+
+int main(){
+
+	printf("Openning...\n");
+	#if defined (NMPU0) && defined (NMPU1)
+	if (ubcOpen("main0.abs","main1.abs",NULL))
+	#elif  defined (NMPU1)
+	if (ubcOpen("","main1.abs",NULL))		// Load executable file to board, connect to shared memory
+	#else  	
+	if (ubcOpen("main0.abs",NULL))			// Load executable file to board, connect to shared memory
+	#endif	
+	{	
+		::Sleep(10000);
+		return -1;
+	}
+	
+	
+	unsigned sync;
+	#ifdef NMPU1
+	ubcGetResult(&sync,1);					// get return value (0x600D)
+	#else 	
+	ubcGetResult(&sync);					// get return value (0x600D)
+	#endif	
+
+	printf("Return value:%X\n",sync);
+	::Sleep(10000);
+	ubcClose();								// close board, disconect from shared memory
+	::Sleep(1000);
+	return 0;
+
+}
